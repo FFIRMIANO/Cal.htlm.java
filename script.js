e8ec3d45b93ca0c277cd701bfbe3742b53e51c94
TATIANE JANUARIA FIRMIANO <110340873+FFIRMIANO@users.noreply.github.com>
2023-06-15 11:14:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcular (params) {
+    
+var base = document.getElementById('base').value
+var altura = document.getElementById('altura').value
+var res = document.getElementById('res')
+ var area = base * altura 
+ 
+ res.innerHTML = `Area calculada  \u{1f449}  ${area} metros`
+}
+
+
+    
+
+
+
+
+
+
